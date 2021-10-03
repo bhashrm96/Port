5753580ff8b6faac689402d3faa42e294ba3fd9a
Gauravcodeit <adhikarigaurav82@gmail.com>
2021-10-03 20:10:56 +0530

    portfolio

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,17 +3,8 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff7510F7);
 const kSecondaryColor = Color(0xff141C3A);
 
+const kCardHeadingStyle = TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
 
-const kCardHeadingStyle = TextStyle(
-  fontSize: 24,
-  fontWeight: FontWeight.bold
-);
+const kCardSubHeadingStyle = TextStyle(fontSize: 18, color: kPrimaryColor);
 
-const kCardSubHeadingStyle = TextStyle(
-  fontSize: 18,
-  color: kPrimaryColor
-);
-
-const kCardTextStyle = TextStyle(
-  fontSize: 18
-);
+const kCardTextStyle = TextStyle(fontSize: 18);
